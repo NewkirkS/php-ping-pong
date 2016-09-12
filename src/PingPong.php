51a91b4aa578afde566d664dc2b5093578e86128
Stephen Newkirk <newkirk771@gmail.com>
2016-09-12 10:39:56 -0700

    Second Test(Divide by 15): green stage pass. No refactor needed.

--- src/PingPong.php
+++ src/PingPong.php
@@ -7,8 +7,13 @@
             $input_number = $input;
             $list = array();
             while ($input_number > 0) {
+                if (($input_number % 15) == 0) {
+                    array_push($list, "Ping-Pong");
+                    --$input_number;
+                } else {
                 array_push($list, $input_number);
                 --$input_number;
+                }
             }
             return implode(" ", array_reverse($list));
         }
