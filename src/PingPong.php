7da01d59a8a30ea7fd53182f2778b29caf9c1037
Stephen Newkirk <newkirk771@gmail.com>
2016-09-12 10:13:29 -0700

    First test: green stage pass. No refactor needed at this time.

--- src/PingPong.php
+++ src/PingPong.php
@@ -4,7 +4,13 @@
     {
         function generatePingPongArray($input)
         {
-            
+            $input_number = $input;
+            $list = array();
+            while ($input_number > 0) {
+                array_push($list, $input_number);
+                --$input_number;
+            }
+            return implode(" ", array_reverse($list));
         }
     }
  ?>
